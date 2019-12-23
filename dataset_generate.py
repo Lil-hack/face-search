d4a7deb930051485d1a8fd93747ac797ff1c04fd
Zikboy <43441041+akashkinKV@users.noreply.github.com>
2019-12-23 16:17:18 +0000

    fisrt commit

---
+++ dataset_generate.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+import pafy
+
+
+if __name__ == '__main__':
+    list_video_url=[]
+    list_video_url.append('https://www.youtube.com/watch?v=FBA3xz0wJTY')
+    list_video_url.append('https://www.youtube.com/watch?v=FBA3xz0wJTY')
+    list_video_url.append('https://www.youtube.com/watch?v=FBA3xz0wJTY')
+    list_video_url.append('https://www.youtube.com/watch?v=FBA3xz0wJTY')
+    list_video_url.append('https://www.youtube.com/watch?v=FBA3xz0wJTY')
+
+
+    count = 0
+    i=0
+    for url in list_video_url:
+        videoPafy = pafy.new(url)
+        best = videoPafy.getbest(preftype="webm")
+        video = cv2.VideoCapture(best.url)
+        while (True):
+            ret, frame = video.read()
+            if ret:
+                cv2.imwrite('dataset/face{:d}.jpg'.format(i), frame)
+                count += 60
+                video.set(1, count)
+                i += 1
+            else:
+                break
+
+
+
---
+++ testing_face_detectors.py
@@ -0,0 +1,28 @@
+import time
+
+import numpy as np
+import cv2
+
+
+face_cascade = cv2.CascadeClassifier ('haarcascade_frontalface_default.xml')
+result_time=0
+count_face=0
+for i in range(0,100):
+    resp = cv2.imread('dataset/face{}.jpg'.format(i), cv2.COLOR_BGR2GRAY)
+    t0 = time.time()
+    faces = face_cascade.detectMultiScale(resp, 1.3, 5, minSize=(30, 30))
+    print (faces)
+    for (x, y, w, h) in faces:
+        r = max(w, h) / 2
+        centerx = x + w / 2
+        centery = y + h / 2
+        nx = int(centerx - r)
+        ny = int(centery - r)
+        nr = int(r * 2)
+        faceimg = resp[ny:ny + nr, nx:nx + nr]
+
+    result_time+=time.time()-t0
+
+
+print(result_time)
+
