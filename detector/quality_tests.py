38b26ea98ed3a315a68318ab0143c8195ea5dc09
kostya <zik1554@bk.ru>
2019-12-24 06:48:39 -0800

    tests update

--- detector/quality_tests.py
+++ detector/quality_tests.py
@@ -1,67 +1,42 @@
-import time
-import numpy as np
 import cv2
+import csv
 import face_recognition
 from mtcnn.mtcnn import MTCNN
-import os.path
-
-detector_folder= os.path.join('detector')
-dataset_folder=os.path.join('dataset')
 
 
 if __name__ == '__main__':
 
-    quality_test=2
+    quality_test=1
 
     if quality_test==1:
-
-        face_cascade = cv2.CascadeClassifier(os.path.join(detector_folder,'haarcascade_frontalface_default.xml'))
-        result_time=0
+        face_cascade = cv2.CascadeClassifier ('detector/haarcascade_frontalface_default.xml')
         count_face=0
         for i in range(1,1000):
-            resp = cv2.imread(os.path.join(dataset_folder,'face{:d}.jpg'.format(i)), cv2.COLOR_BGR2GRAY)
-            t0 = time.time()
+            resp = cv2.imread('dataset/face{}.jpg'.format(i), cv2.COLOR_BGR2GRAY)
             faces = face_cascade.detectMultiScale(resp, 1.3, 5, minSize=(30, 30))
-            print (faces)
-            for (x, y, w, h) in faces:
-                r = max(w, h) / 2
-                centerx = x + w / 2
-                centery = y + h / 2
-                nx = int(centerx - r)
-                ny = int(centery - r)
-                nr = int(r * 2)
-                faceimg = resp[ny:ny + nr, nx:nx + nr]
+            count_face+=len(faces)
+            with open('names.csv') as csvfile:
+                reader = csv.DictReader(csvfile)
+            for row in reader:
+                print(row['first_name'], row['last_name'])
 
-            result_time+=time.time()-t0
-        print(result_time)
+        print (count_face)
 
     if quality_test==2:
-        result_time=0
         count_face=0
         detector = MTCNN()
         for i in range(1,1000):
             img = cv2.imread('dataset/face{}.jpg'.format(i), cv2.COLOR_BGR2GRAY)
-
-            t0 = time.time()
             faces = detector.detect_faces(img)
-
-            for face in faces:
-                # print(face['box'])
-                pass
-            result_time+=time.time()-t0
-            print(faces)
-
-        print(result_time)
+            count_face+=len(faces)
+        print (count_face)
 
     if quality_test==3:
-        result_time = 0
         count_face = 0
         detector = MTCNN()
         for i in range(1, 1000):
             img = cv2.imread('dataset/face{}.jpg'.format(i), cv2.COLOR_BGR2GRAY)
-            t0 = time.time()
-            face_locations = face_recognition.face_locations(img)
-            result_time += time.time() - t0
-            print(face_locations)
+            faces = face_recognition.face_locations(img)
+            count_face += len(faces)
+        print(count_face)
 
-        print(result_time)
--- detector/speed_tests.py
+++ detector/speed_tests.py
@@ -1,5 +1,4 @@
 import time
-import numpy as np
 import cv2
 import face_recognition
 from mtcnn.mtcnn import MTCNN
@@ -7,41 +6,26 @@ from mtcnn.mtcnn import MTCNN
 
 if __name__ == '__main__':
 
-    speed_test=1
+    speed_test=2
 
     if speed_test==1:
         face_cascade = cv2.CascadeClassifier ('detector/haarcascade_frontalface_default.xml')
         result_time=0
-        count_face=0
         for i in range(1,1000):
             resp = cv2.imread('dataset/face{}.jpg'.format(i), cv2.COLOR_BGR2GRAY)
             t0 = time.time()
             faces = face_cascade.detectMultiScale(resp, 1.3, 5, minSize=(30, 30))
-            print (faces)
-            for (x, y, w, h) in faces:
-                r = max(w, h) / 2
-                centerx = x + w / 2
-                centery = y + h / 2
-                nx = int(centerx - r)
-                ny = int(centery - r)
-                nr = int(r * 2)
-                faceimg = resp[ny:ny + nr, nx:nx + nr]
 
             result_time+=time.time()-t0
         print(result_time)
 
     if speed_test==2:
         result_time=0
-        count_face=0
         detector = MTCNN()
         for i in range(1,1000):
             img = cv2.imread('dataset/face{}.jpg'.format(i), cv2.COLOR_BGR2GRAY)
             t0 = time.time()
             faces = detector.detect_faces(img)
-
-            for face in faces:
-                # print(face['box'])
-                pass
             result_time+=time.time()-t0
             print(faces)
 
@@ -49,7 +33,6 @@ if __name__ == '__main__':
 
     if speed_test==3:
         result_time = 0
-        count_face = 0
         detector = MTCNN()
         for i in range(1, 1000):
             img = cv2.imread('dataset/face{}.jpg'.format(i), cv2.COLOR_BGR2GRAY)
