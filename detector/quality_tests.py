9156913ba103fbb7aa5512820caeaa8c659b26eb
kostya <zik1554@bk.ru>
2019-12-24 05:54:07 -0800

    quality test and dataset info csv

--- detector/quality_tests.py
+++ detector/quality_tests.py
@@ -3,18 +3,23 @@ import numpy as np
 import cv2
 import face_recognition
 from mtcnn.mtcnn import MTCNN
+import os.path
+
+detector_folder= os.path.join('detector')
+dataset_folder=os.path.join('dataset')
 
 
 if __name__ == '__main__':
 
-    speed_test=1
+    quality_test=2
 
-    if speed_test==1:
-        face_cascade = cv2.CascadeClassifier ('haarcascade_frontalface_default.xml')
+    if quality_test==1:
+
+        face_cascade = cv2.CascadeClassifier(os.path.join(detector_folder,'haarcascade_frontalface_default.xml'))
         result_time=0
         count_face=0
         for i in range(1,1000):
-            resp = cv2.imread('dataset/face{}.jpg'.format(i), cv2.COLOR_BGR2GRAY)
+            resp = cv2.imread(os.path.join(dataset_folder,'face{:d}.jpg'.format(i)), cv2.COLOR_BGR2GRAY)
             t0 = time.time()
             faces = face_cascade.detectMultiScale(resp, 1.3, 5, minSize=(30, 30))
             print (faces)
@@ -30,12 +35,13 @@ if __name__ == '__main__':
             result_time+=time.time()-t0
         print(result_time)
 
-    if speed_test==2:
+    if quality_test==2:
         result_time=0
         count_face=0
         detector = MTCNN()
         for i in range(1,1000):
             img = cv2.imread('dataset/face{}.jpg'.format(i), cv2.COLOR_BGR2GRAY)
+
             t0 = time.time()
             faces = detector.detect_faces(img)
 
@@ -47,7 +53,7 @@ if __name__ == '__main__':
 
         print(result_time)
 
-    if speed_test==3:
+    if quality_test==3:
         result_time = 0
         count_face = 0
         detector = MTCNN()
