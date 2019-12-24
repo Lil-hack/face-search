e080ac41dc11a5887b2f6b0279f705065269de4a
kostya <zik1554@bk.ru>
2019-12-24 07:31:55 -0800

    tests updates ver2

--- detector/quality_tests.py
+++ detector/quality_tests.py
@@ -11,32 +11,44 @@ if __name__ == '__main__':
     if quality_test==1:
         face_cascade = cv2.CascadeClassifier ('detector/haarcascade_frontalface_default.xml')
         count_face=0
-        for i in range(1,1000):
-            resp = cv2.imread('dataset/face{}.jpg'.format(i), cv2.COLOR_BGR2GRAY)
-            faces = face_cascade.detectMultiScale(resp, 1.3, 5, minSize=(30, 30))
-            count_face+=len(faces)
-            with open('names.csv') as csvfile:
-                reader = csv.DictReader(csvfile)
-            for row in reader:
-                print(row['first_name'], row['last_name'])
+        with open('dataset_haar.csv', 'w') as csvfile:
+            fieldnames = ['N', 'Haar']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for i in range(1, 1000):
+                resp = cv2.imread('dataset/face{}.jpg'.format(i), cv2.COLOR_BGR2GRAY)
+                faces = face_cascade.detectMultiScale(resp, 1.3, 5, minSize=(30, 30))
+                count_face += len(faces)
+                writer.writerow({'N': i, 'Haar': len(faces)})
 
         print (count_face)
 
     if quality_test==2:
         count_face=0
         detector = MTCNN()
-        for i in range(1,1000):
-            img = cv2.imread('dataset/face{}.jpg'.format(i), cv2.COLOR_BGR2GRAY)
-            faces = detector.detect_faces(img)
-            count_face+=len(faces)
+        with open('dataset_tenserflow.csv', 'w') as csvfile:
+            fieldnames = ['N', 'Tenser']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for i in range(1, 1000):
+                img = cv2.imread('dataset/face{}.jpg'.format(i), cv2.COLOR_BGR2GRAY)
+                faces = detector.detect_faces(img)
+                count_face += len(faces)
+                writer.writerow({'N': i, 'Tenser': len(faces)})
+
         print (count_face)
 
     if quality_test==3:
         count_face = 0
-        detector = MTCNN()
-        for i in range(1, 1000):
-            img = cv2.imread('dataset/face{}.jpg'.format(i), cv2.COLOR_BGR2GRAY)
-            faces = face_recognition.face_locations(img)
-            count_face += len(faces)
+        with open('dataset_dlib.csv', 'w') as csvfile:
+            fieldnames = ['N', 'Haar']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for i in range(1, 1000):
+                img = cv2.imread('dataset/face{}.jpg'.format(i), cv2.COLOR_BGR2GRAY)
+                faces = face_recognition.face_locations(img)
+                count_face += len(faces)
+                writer.writerow({'N': i, 'Dlib': len(faces)})
+
         print(count_face)
 
--- detector/speed_tests.py
+++ detector/speed_tests.py
@@ -6,7 +6,7 @@ from mtcnn.mtcnn import MTCNN
 
 if __name__ == '__main__':
 
-    speed_test=2
+    speed_test=3
 
     if speed_test==1:
         face_cascade = cv2.CascadeClassifier ('detector/haarcascade_frontalface_default.xml')
@@ -15,7 +15,7 @@ if __name__ == '__main__':
             resp = cv2.imread('dataset/face{}.jpg'.format(i), cv2.COLOR_BGR2GRAY)
             t0 = time.time()
             faces = face_cascade.detectMultiScale(resp, 1.3, 5, minSize=(30, 30))
-
+            print(faces)
             result_time+=time.time()-t0
         print(result_time)
 
@@ -33,7 +33,6 @@ if __name__ == '__main__':
 
     if speed_test==3:
         result_time = 0
-        detector = MTCNN()
         for i in range(1, 1000):
             img = cv2.imread('dataset/face{}.jpg'.format(i), cv2.COLOR_BGR2GRAY)
             t0 = time.time()
