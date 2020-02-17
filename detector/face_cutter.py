3f46c73a62915f15dd80bcde7aa9830501bf1494
zikboy <zik1554@bk.ru>
2020-02-17 00:25:55 -0800

    tensorflow train people createdataset

--- detector/face_cutter.py
+++ detector/face_cutter.py
@@ -30,21 +30,20 @@ def crop_face(img,detector):
 if __name__ == '__main__':
 
     detector = MTCNN()
+    folder='women'
     i=0
-    for list_photos in os.walk('/home/zikboy/Documents/data/men2'):
+    for list_photos in os.walk('/home/zikboy/Documents/data/{}'.format(folder)):
             for photo in list_photos[2]:
                 i=i+1
-                if i>0:
-                    img = cv2.imread('/home/zikboy/Documents/data/men2/{}'.format(photo), cv2.COLOR_BGR2GRAY)
+                if i<100000:
+                    img = cv2.imread('/home/zikboy/Documents/data/{}/{}'.format(folder,photo), cv2.COLOR_BGR2GRAY)
                     cropped = crop_face(img, detector)
                     # print (img)
                     # print('')
                     try:
-                        norm_face=cv2.resize(cropped, (200, 200), interpolation=cv2.INTER_LINEAR)
+                        norm_face=cv2.resize(cropped, (200, 200),fx=0.3,fy=0.3, interpolation=cv2.INTER_AREA)
                         # norm_face=cv2.normalize(norm_face, norm_face, 0, 255, cv2.NORM_MINMAX)
-
-                        cv2.imwrite(r'{}.jpg'.format(uuid.uuid1()), norm_face)
-                        Thread.sleep(5)
+                        cv2.imwrite(r'{}/{}.jpg'.format(folder,uuid.uuid1()), norm_face)
                     except:
                         pass
 
